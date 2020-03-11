d29e95283ece8f107f3754e3ac11a2f74f409394
Derek J. Dalle <derek.j.dalle@nasa.gov>
2020-03-10 23:11:06 -0700

    Initial commit

---
+++ copy-files.py
@@ -0,0 +1,56 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# Standard library modules
+import os
+import shutil
+
+
+# This folder
+fdir = os.path.dirname(os.path.realpath(__file__))
+# Name of working folder
+fwork = os.path.join(fdir, "work")
+
+# Files list
+fnames = [
+    "pyCart.json",
+    "inputs/input.cntl",
+    "inputs/aero.csh",
+    "inputs/bJet.i.tri",
+    "inputs/Config.xml",
+    "inputs/matrix.csv"
+]
+
+# Check if working folder exists
+if not os.path.isdir(fwork):
+    # Create it
+    os.mkdir(fwork)
+
+# Loop through files
+for fname in fnames:
+    # System independence
+    f1 = fname.replace("/", os.sep)
+    # Source file
+    fsrc = os.path.join(fdir, f1)
+    # Destination file
+    fdest = os.path.join(fwork, f1)
+    # Check if output file already exists
+    if os.path.isfile(fdest):
+        # Skip
+        continue
+    # Break out folder names (if any)
+    fparts = fname.split("/")[:-1]
+    # Create cumulative subfolder name
+    fsub = fwork
+    # Loop through subfolder names
+    for fpart in fparts:
+        # Add to subfolder name
+        fsub = os.path.join(fsub, fpart)
+        # Check if folder exists
+        if os.path.isdir(fsub):
+            continue
+        # Create folder
+        os.mkdir(fsub)
+    # Copy file
+    shutil.copy(fsrc, fdest)
+
